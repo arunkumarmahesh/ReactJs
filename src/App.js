a5eaa33a305fe6fda9a5389613d47d9255cfa35b
Arun Mahesh <mahesharunkumar.ku@gmail.com>
2020-04-24 10:58:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hurrah...!!!! my first app on web ;) ;) ;)
         </a>
       </header>
     </div>
